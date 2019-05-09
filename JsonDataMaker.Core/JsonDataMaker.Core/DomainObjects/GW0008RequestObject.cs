dcfd9ea25f32b091b994fc0c5c461834e7a92541
Shigezo <u.sake32@gmail.com>
2019-05-09 17:54:11 +0900

    feat: GW0008リクエスト用のパーサーを作成

---
+++ JsonDataMaker.Core/JsonDataMaker.Core/DomainObjects/GW0008RequestObject.cs
@@ -0,0 +1,13 @@
+﻿using BankVision.WebAPI.Models.GW0008.Request;
+
+namespace JsonDataMaker.Core.DomainObjects
+{
+    public class GW0008RequestObject
+    {
+        /// <summary>
+        /// ファイルID
+        /// </summary>
+        public int FileId { get; set; }
+        public RequestMessageData RequestMessageData { get; set; }
+    }
+}
