aae13e6a9a5ae377a7514386527ab463498ecec8
Shigezo <u.sake32@gmail.com>
2019-05-10 12:37:28 +0900

    feat: JsonWriterを作成

---
+++ JsonDataMaker.Core/JsonDataMaker.Core/IJsonWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonDataMaker.Core
+{
+    public interface IJsonWriter
+    {
+        Task<Stream> CreateAsync(object value, string path);
+    }
+}
---
+++ JsonDataMaker.Core/JsonDataMaker.Core/JsonFileWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JsonDataMaker.Core
+{
+    public class JsonFileWriter : IJsonWriter
+    {
+        public async Task<Stream> CreateAsync(object value, string path)
+        {
+            var writer = File.CreateText(path);
+            await writer.WriteAsync(JsonConvert.SerializeObject(value, Formatting.Indented));
+            await writer.FlushAsync();
+            return writer.BaseStream;
+        }
+    }
+}
