e8f908484fdfe4f2b5090e1e5626064b5e740247
Shigezo <u.sake32@gmail.com>
2019-05-13 14:50:00 +0900

    feat: GW0008 レスポンス用のパーサーを実装

---
+++ JsonDataMaker.Core/JsonDataMaker.Core/Parsers/GW0008ResponseCsvParser.cs
@@ -0,0 +1,10 @@
+﻿using BankVision.WebAPI.Models.GW0008.Response;
+using JsonDataMaker.Core.DomainObjects.Csv;
+using JsonDataMaker.Core.Maps;
+
+namespace JsonDataMaker.Core.Parsers
+{
+    public class GW0008ResponseCsvParser : CsvParser<Root<ResponseMessageData>, GW0008ResponseMap>
+    {
+    }
+}
