b9994a44e5970563463d10f2a05de5937a693c72
nakagawa.saki <mn.qdbp@gmail.com>
2019-05-13 13:45:13 +0900

    指摘事項修正

--- JsonDataMaker.Core/JsonDataMaker.Core/DomainObjects/GW1001RequestObject.cs
+++ JsonDataMaker.Core/JsonDataMaker.Core/DomainObjects/GW1001RequestObject.cs
@@ -1,26 +1,10 @@
+using BankVision.WebAPI.Models.GW1001.Request;
+
 namespace JsonDataMaker.Core.DomainObjects
 {
     public class GW1001RequestObject
     {
-        /// <summary>
-        /// ファイルID
-        /// </summary>
         public int FileId { get; set; }
-        /// <summary>
-        /// 契約者ID
-        /// </summary>
-        public int keiyakushaId { get; set; }
-        /// <summary>
-        /// 店番
-        /// </summary>
-        public int temban { get; set; }
-        /// <summary>
-        /// 科目コード
-        /// </summary>
-        public int kamokuCode { get; set; }
-        /// <summary>
-        /// 口座番号
-        /// </summary>
-        public int kozabango { get; set; }
+        public RequestMessageData RequestMessageData { get; set; }
     }
 }
--- JsonDataMaker.Core/JsonDataMaker.Core/Maps/GW1001RequestMap.cs
+++ JsonDataMaker.Core/JsonDataMaker.Core/Maps/GW1001RequestMap.cs
@@ -8,10 +8,10 @@ namespace JsonDataMaker.Core.Maps
         public GW1001RequestMap()
         {
             Map(x => x.FileId).Index(0).Name("FileId");
-            Map(x => x.keiyakushaId).Index(1).Name("keiyakushaId");
-            Map(x => x.temban).Index(2).Name("temban");
-            Map(x => x.kamokuCode).Index(3).Name("kamokuCode");
-            Map(x => x.kozabango).Index(4).Name("kozabango");
+            Map(x => x.RequestMessageData.BizIbKeiyakushaJohoShokai.keiyakushaId).Index(1).Name("keiyakushaId");
+            Map(x => x.RequestMessageData.BizIbKeiyakushaJohoShokai.temban).Index(2).Name("temban");
+            Map(x => x.RequestMessageData.BizIbKeiyakushaJohoShokai.kamokuCode).Index(3).Name("kamokuCode");
+            Map(x => x.RequestMessageData.BizIbKeiyakushaJohoShokai.kozabango).Index(4).Name("kozabango");
         }
     }
 }
