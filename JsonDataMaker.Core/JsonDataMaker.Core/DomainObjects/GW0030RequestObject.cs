959f8e53f61b0d38410d3a1638c8fc9bae3cab9d
jumpei.okumura <b1332751@gmail.com>
2019-05-21 15:38:27 +0900

    GW0030 Request

---
+++ JsonDataMaker.Core/JsonDataMaker.Core/DomainObjects/GW0030RequestObject.cs
@@ -0,0 +1,9 @@
+﻿using BankVision.WebAPI.Models.GW0030.Request;
+using JsonDataMaker.Core.DomainObjects.Csv;
+
+namespace JsonDataMaker.Core.DomainObjects
+{
+    public class GW0030RequestObject : Root<RequestMessageData>
+    {
+    }
+}
